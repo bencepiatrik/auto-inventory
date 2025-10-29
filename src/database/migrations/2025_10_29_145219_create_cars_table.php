f5167417823e03cb8eb7e02325fb3dbb8ce280f4
bencepiatrik <bence.piatrik@azet.sk>
2025-10-29 16:17:36 +0100

    Dockerized laravel setup + car/parts migrations

---
+++ src/database/migrations/2025_10_29_145219_create_cars_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // required
+            $table->string('registration_number')->nullable(); // required only if is_registered = true
+            $table->boolean('is_registered')->default(false);
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
