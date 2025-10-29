8297d0ad2f8c3a470ac3d0084d090609d3486fa7
bencepiatrik <bence.piatrik@azet.sk>
2025-10-29 21:44:33 +0100

    Car and Part relations, fillable & casts, migration for addded part quantity column

--- src/app/Models/Car.php
+++ src/app/Models/Car.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Car extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'registration_number',
+        'is_registered',
+    ];
+
+    protected $casts = [
+        'is_registered' => 'boolean',
+    ];
+
+    public function parts(): HasMany
+    {
+        return $this->hasMany(Part::class);
+    }
 }
--- src/app/Models/Part.php
+++ src/app/Models/Part.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Part extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'serialnumber',
+        'quantity',   // nový stĺpec
+        'car_id',
+    ];
+
+    public function car(): BelongsTo
+    {
+        return $this->belongsTo(Car::class);
+    }
 }
